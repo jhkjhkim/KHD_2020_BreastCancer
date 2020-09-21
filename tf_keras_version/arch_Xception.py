33f67f4d3083896868bf03523ee084eeefced31f
DMkelllog <kelllog@naver.com>
2020-09-21 15:14:26 +0900

    arch_Xception

---
+++ tf_keras_version/arch_Xception.py
@@ -0,0 +1,12 @@
+import tensorflow as tf # Tensorflow 2
+from tensorflow.keras.applications import Xception
+
+def cnn():
+    conv_base = Xception(weights='imagenet',
+                         include_top = False)
+    model = tf.keras.models.Sequential()
+    model.add(conv_base)
+    model.add(tf.keras.layers.GlobalAveragePooling2D())
+    model.add(tf.keras.layers.Dense(512, activation='relu'))
+    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+    return model
