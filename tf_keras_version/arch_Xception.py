5fe789a156508fd20e7cc19a9fb6dbf372e04e8a
DMkelllog <kelllog@naver.com>
2020-09-21 15:20:26 +0900

    arch_Xception

--- tf_keras_version/arch_Xception.py
+++ tf_keras_version/arch_Xception.py
@@ -1,12 +1,37 @@
 import tensorflow as tf # Tensorflow 2
 from tensorflow.keras.applications import Xception
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, BatchNormalization, Activation, Dropout, GlobalAveragePooling2D
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+import csv
+import sys
+import itertools
 
-def Xception():
-    conv_base = Xception(weights='imagenet',
-                         include_top = False)
-    model = tf.keras.models.Sequential()
-    model.add(conv_base)
-    model.add(tf.keras.layers.GlobalAveragePooling2D())
-    model.add(tf.keras.layers.Dense(512, activation='relu'))
-    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+def build_xception():
+    base_model = Xception(include_top = False, weights='imagenet', input_shape = (299,299,3))
+    base_model.trainable = True
+    
+    base_model.outputs = [base_model.layers[-1].output]
+    last = base_model.outputs[0]
+    x = GlobalAveragePooling2D()(last)
+    preds = Dense(1, activation='sigmoid')(x)
+    
+    model = Model(base_model.input, preds)
+    
+    model.compile(loss = 'binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
     return model
+
+def build_train_generator(train_dir, target_image_size, train_batch_size):
+    datagen = ImageDataGenerator(horizontal_flip=True,
+                                vertical_flip=True,
+                                rescale = 1./255)
+    
+    train_generator = datagen.flow_from_directory(train_dir,
+                                                  target_size = target_image_size, 
+                                                  batch_size=train_batch_size,
+                                                 class_mode = 'binary')
+    
+    
+    return train_generator
